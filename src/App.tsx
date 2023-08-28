1280097714a187b0e74d4737afde94ff49a1f052
Daryl Djelou <daryl.djelou@gmail.com>
2023-08-28 16:42:19 +0200

    fix(promise issue)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import axios from "axios";
 import SearchBar from "./components/searchBar";
 
-async function get_data_array(): Promise<any[]> {
-  const response = await axios.get(
-    "https://hds-staging.toktokdoc.com/physicians?search=_&limit=100"
-  );
-  const Array = [];
-  Array.push(response.data);
-  return Array;
-}
 function App() {
-  const data = get_data_array();
-  console.log(data);
   return (
     <>
-      <div>
-        <SearchBar />
-        {data[0]["identifiantpp"]}
-      </div>
+      <div></div>
     </>
   );
 }
--- src/components/PhysicianList.tsx
+++ src/components/PhysicianList.tsx
@@ -5,7 +5,12 @@ interface PhysiciansProps {
   name: string;
 }
 
-async function setPhysicianList(url: string): PhysiciansProps[] {
+interface PhysicianListProps {
+  input: string;
+  url: string;
+}
+
+async function setPhysicianList(url: string): Promise<PhysiciansProps[]> {
   const response = await axios.get(url);
   const PhysicianArray: PhysiciansProps[] = [];
   const dataArray = [];
@@ -13,14 +18,28 @@ async function setPhysicianList(url: string): PhysiciansProps[] {
   dataArray.map((data) => {
     const PhysicianObject: PhysiciansProps = {
       identifiantpp: data["identifiantpp"],
-      name: data["prenomd'exercice"],
+      name: data["prenomdexercice"],
     };
     PhysicianArray.push(PhysicianObject);
   });
   return PhysicianArray;
 }
-const PhysicianList = () => {
-  return <div>PhysicianList</div>;
-};
+async function PhysicianList({ input, url }: PhysicianListProps) {
+  const PhysicianArray = await setPhysicianList(url);
+  const ValidArray = PhysicianArray.filter(
+    (physician) =>
+      physician.identifiantpp.includes(input) || physician.name.includes(input)
+  );
+
+  return (
+    <div>
+      {ValidArray.map((data) => (
+        <h1>
+          {data.identifiantpp} {data.name}
+        </h1>
+      ))}
+    </div>
+  );
+}
 
 export default PhysicianList;
